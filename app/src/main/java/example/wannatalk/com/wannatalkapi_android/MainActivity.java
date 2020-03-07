df716c8c7df366aeed1d1feaa3d7a30c5fdd87e8
Srikanth Ganji <srikanth.ganji@live.com>
2020-03-07 15:10:38 +0530

    #MI5AI0A

--- app/src/main/java/example/wannatalk/com/wannatalkapi_android/MainActivity.java
+++ app/src/main/java/example/wannatalk/com/wannatalkapi_android/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WTSDKManager.InitializeSDK();
+//        WTSDKManager.InitializeSDK();
 
         WTSDKManager.ShowGuideButton(false);
         WTSDKManager.ShowProfileInfoPage(false);
